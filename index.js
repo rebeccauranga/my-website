94b6a5900b5d96753d8063cba16960b442b61b34
Rebecca Uranga <urangarebecca@gmail.com>
2019-04-22 12:47:33 -0400

    adds hover feature on description text

--- index.js
+++ index.js
@@ -23,21 +23,39 @@ function onLoad() {
 
 
 function showDescription(event){
-  // console.log(event.target.id);
-  // var job = document.querySelector(`[data-company-id='${event.target.id}']`);
-  // // alert(job.innerHTML);
-  // job.classList.toggle('show-description');  
-
-
-    const detailArea = document.querySelector('[data-detail]');
-
-    const descriptionDiv = document.createElement('div');
-
-    descriptionDiv.textContent = `[data-company-id='${event.target.id}']`;
-
-    detailArea.appendChild(descriptionDiv);
+  console.log(event.target.id);
+  var job = document.querySelector(`[data-company-id='${event.target.id}']`);
+  // alert(job.innerHTML);
+  job.classList.toggle('show-description');  
 }
 
+function changeDCTextColor(event) {
+  document.getElementById("digitalcrafts").style.color = "#63FFE7";
+  document.getElementById("ew").style.color = "#FFFFFF";
+  document.getElementById("cm").style.color = "#FFFFFF";
+  document.getElementById("uwg").style.color = "#FFFFFF";
+}
+
+function changeEWTextColor(event) {
+  document.getElementById("digitalcrafts").style.color = "#FFFFFF";
+  document.getElementById("ew").style.color = "#63FFE7";
+  document.getElementById("cm").style.color = "#FFFFFF";
+  document.getElementById("uwg").style.color = "#FFFFFF";
+}
+
+function changeCMTextColor(event) {
+  document.getElementById("digitalcrafts").style.color = "#FFFFFF";
+  document.getElementById("ew").style.color = "#FFFFFF";
+  document.getElementById("cm").style.color = "#63FFE7";
+  document.getElementById("uwg").style.color = "#FFFFFF";
+}
+
+function changeUWGTextColor(event) {
+  document.getElementById("digitalcrafts").style.color = "#FFFFFF";
+  document.getElementById("ew").style.color = "#FFFFFF";
+  document.getElementById("cm").style.color = "#FFFFFF";
+  document.getElementById("uwg").style.color = "#63FFE7";
+}
 
 
 window.addEventListener("scroll", onScroll);
